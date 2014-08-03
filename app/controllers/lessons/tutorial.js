67d837a594a055f734ef7f6d3436ddfc1da467b5
Jonathan <laptopfreek0@gmail.com>
2014-08-03 11:05:12 -0400

    Added proper source, and browser toggle, and rendering.

--- app/controllers/lessons/tutorial.js
+++ app/controllers/lessons/tutorial.js
@@ -29,12 +29,35 @@ function toggleTab(source) {
 		$.lblSource.setColor("#FFF");
 		$.btnBrowser.setBackgroundColor("#d3d3d3");
 		$.lblBrowser.setColor("#000");
+		showSource();
 	} else {
 		$.btnSource.setBackgroundColor("#d3d3d3");
 		$.lblSource.setColor("#000");
 		$.btnBrowser.setBackgroundColor("#33b5e5");
 		$.lblBrowser.setColor("#FFF");
+		showBrowser();
 	}
+}
+
+function showSource() {
+	$.sourceWrapper.removeAllChildren();
+	var lblSource = Ti.UI.createLabel({
+		text: tutorial.example.source,
+		left: 10,
+		top: 10
+	});
+	
+	$.sourceWrapper.add(lblSource);
+}
+
+function showBrowser() {
+	$.sourceWrapper.removeAllChildren();
+	Log.info("HTML source: " + tutorial.example.html);
+	var browser = Ti.UI.createWebView({
+		html: tutorial.example.html
+	});
+	
+	$.sourceWrapper.add(browser);
 }
 
 init(arguments[0] || {});
