edb17adb7287a3dd1a7d145a2aa953e091cae23c
laptopfreek0 <laptopfreek0@yahoo.com>
2014-08-23 12:00:37 -0400

    Accidently changed the wrong quiz type to autoscroll.

--- app/controllers/lessons/questions/code.js
+++ app/controllers/lessons/questions/code.js
@@ -100,7 +100,6 @@ function clickQuestionButton(e) {
 	button.addEventListener('click', clickAnswerButton);
 	answerButtons.push(button);
 	$.answerBox.add(button);
-	$.answerBox.scrollToBottom();
 	//e.source.setVisible(false);
 }
 
