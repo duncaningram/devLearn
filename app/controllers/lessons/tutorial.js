c9dc29abb057de1549b601ce3de61865e3364259
Tim Coy <timothycoy@gmail.com>
2014-09-08 19:05:18 -0400

    Added support for CSS and JS to the tutorial page.

--- app/controllers/lessons/tutorial.js
+++ app/controllers/lessons/tutorial.js
@@ -12,37 +12,85 @@ function init(args) {
 	
 	$.txtTitle.setText(tutorial.title);
 	$.txtDescription.setText(tutorial.description);
-	$.txtSource.setText(tutorial.example.source);
-	$.btnSource.addEventListener('click', function() {
-		toggleTab(true);
-	});
-	$.btnBrowser.addEventListener('click', function() {
-		toggleTab(false);
-	});
 	
-
+	if (tutorial.example.source) {
+		$.btnSource.addEventListener('click', function() { toggleTab("source"); });
+	} else {
+		$.tabs.remove($.btnSource);
+	}
+	
+	if (tutorial.example.source_css) {
+		$.btnSourceCSS.addEventListener('click', function() { toggleTab("source_css"); });
+	} else {
+		$.tabs.remove($.btnSourceCSS);
+	}
+	
+	if (tutorial.example.source_js) {
+		$.btnSourceJS.addEventListener('click', function() { toggleTab("source_js"); });
+	} else {
+		$.tabs.remove($.btnSourceJS);
+	}
+	
+	if (tutorial.example.html) {
+		$.btnBrowser.addEventListener('click', function() { toggleTab("html"); });
+	} else {
+		$.tabs.remove($.btnBrowser);
+	}
+	
+	showSource(tutorial.example.source);
 }
 
 function toggleTab(source) {	
-	if (source) {
+	if (source == "source") {
 		$.btnSource.setBackgroundColor("#33b5e5");
 		$.lblSource.setColor("#FFF");
+		$.btnSourceCSS.setBackgroundColor("#d3d3d3");
+		$.lblSourceCSS.setColor("#000");
+		$.btnSourceJS.setBackgroundColor("#d3d3d3");
+		$.lblSourceJS.setColor("#000");
 		$.btnBrowser.setBackgroundColor("#d3d3d3");
 		$.lblBrowser.setColor("#000");
-		showSource();
-	} else {
+		showSource(tutorial.example.source);
+	}
+	else if (source == "source_css") {
 		$.btnSource.setBackgroundColor("#d3d3d3");
 		$.lblSource.setColor("#000");
+		$.btnSourceCSS.setBackgroundColor("#33b5e5");
+		$.lblSourceCSS.setColor("#FFF");
+		$.btnSourceJS.setBackgroundColor("#d3d3d3");
+		$.lblSourceJS.setColor("#000");
+		$.btnBrowser.setBackgroundColor("#d3d3d3");
+		$.lblBrowser.setColor("#000");
+		showSource(tutorial.example.source_css);
+	}
+	else if (source == "source_js") {
+		$.btnSource.setBackgroundColor("#d3d3d3");
+		$.lblSource.setColor("#000");
+		$.btnSourceCSS.setBackgroundColor("#d3d3d3");
+		$.lblSourceCSS.setColor("#000");
+		$.btnSourceJS.setBackgroundColor("#33b5e5");
+		$.lblSourceJS.setColor("#FFF");
+		$.btnBrowser.setBackgroundColor("#d3d3d3");
+		$.lblBrowser.setColor("#000");
+		showSource(tutorial.example.source_js);
+	}
+	else {
+		$.btnSource.setBackgroundColor("#d3d3d3");
+		$.lblSource.setColor("#000");
+		$.btnSourceCSS.setBackgroundColor("#d3d3d3");
+		$.lblSourceCSS.setColor("#000");
+		$.btnSourceJS.setBackgroundColor("#d3d3d3");
+		$.lblSourceJS.setColor("#000");
 		$.btnBrowser.setBackgroundColor("#33b5e5");
 		$.lblBrowser.setColor("#FFF");
 		showBrowser();
 	}
 }
 
-function showSource() {
+function showSource(source) {
 	$.sourceWrapper.removeAllChildren();
 	var lblSource = Ti.UI.createLabel({
-		text: tutorial.example.source,
+		text: source,
 		left: 10,
 		top: 10
 	});
