a8ac0cf3e7b77f4d59b6f133d3fe5c47238930b0
Tim Coy <timothycoy@gmail.com>
2016-09-28 23:19:02 -0400

    Added backendless

--- app/alloy.js
+++ app/alloy.js
@@ -9,3 +9,24 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
+var Backendless = require('vendor/backendless');
+var Log = require('utils/Log');
+
+// TODO: Load from tiapp.xml
+Backendless.initApp('18F03ACC-5BDA-13F2-FF4D-4A027A07FC00', '20C2CC94-A867-F193-FFBB-AB8468264C00', 'v1');
+
+/* Example Registration Code should be in User.js
+function userRegistered(user) {
+    Log.info("user has registered");
+}
+
+function userError(err) {
+    Log.error("error message - " + err.message);
+    Log.error("error code - " + err.statusCode);
+}
+
+var user = new Backendless.User();
+user.email = "tim@test.com";
+user.password = "test123";
+Backendless.UserService.register(user, new Backendless.Async(userRegistered, userError));
+*/
